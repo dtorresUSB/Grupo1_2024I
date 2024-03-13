74f8c236652b0ba060f29d48e971bd2bc1d32249
dtorresUSB <dtorres@usbbog.edu.co>
2024-03-13 11:32:15 -0500

    retroalimentacion2

--- Sesion9/punto3.py
+++ Sesion9/punto3.py
@@ -6,25 +6,21 @@ def matriz_mult(a,b):
     col_b=len(b[0])
 
     val=0
-    txt=''
+    #txt=''
     res=[]
-    for i in range(row_a):
-        res.append([])
-    print(res)
 
     if col_a==row_b:
         for i in range(row_a):
+            res.append([])
             for j in range(col_b):
                 for k in range(col_a):
                     val+=a[i][k]*b[k][j]
-                res[i].append(val)
                     #txt+=f'[{i}][{k}]x[{k}][{j}]'
-                #print(val)
-                #print(txt)
+                res[i].append(val)
                 val=0
+                #print(txt)
                 #txt=''
-        for i in res:
-            print(i)
+            print(res[i])
     else:
         print('error')
 
@@ -33,3 +29,16 @@ if __name__=="__main__":
     a=[[1,2,2,4],[3,4,-1,1],[2,1,0,2]]
     b=[[0,1],[2,3],[1,2],[0,0]]
     matriz_mult(a,b)
+
+# ----------------- Analisis ---------------
+#  i      k      j     i      k      j
+#     ---------           ---------
+# [0] |[0]*[0]| [0] + [0] |[1]*[1]| [0]
+# [0] |[0]*[0]| [1] + [0] |[1]*[1]| [1]
+# [1] |[0]*[0]| [0] + [1] |[1]*[1]| [0]
+# [1] |[0]*[0]| [1] + [1] |[1]*[1]| [1]
+#     ---------           ---------
+    
+# i= rows_a 
+# k= cols_a or rows_b
+# j= cols_b
